424c49c4ec8ca85b066c7e93ebe81157ee3c5c4c
Gabriel Ignacio Baez <45924381+GabrielBaezB@users.noreply.github.com>
2025-01-07 10:42:23 -0300

    Add files via upload

---
+++ muchalucha-backend/src/main/java/com/backend/muchalucha_backend/MuchaluchaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.backend.muchalucha_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuchaluchaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuchaluchaBackendApplication.class, args);
+	}
+
+}
---
+++ muchalucha-backend/src/test/java/com/backend/muchalucha_backend/MuchaluchaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.muchalucha_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuchaluchaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
